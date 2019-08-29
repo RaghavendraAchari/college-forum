ccdfab568c3a46e4954223da4a569c69d426302d
Raghavendra Achari <raghav.achari.l@gmail.com>
2019-08-29 17:40:56 +0530

    Added NavBar and LigIn components

---
+++ src/components/logInForm.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import Input from "./common/input";
+
+class LogInForm extends Component {
+  state = {
+    account: { username: "", password: "" }
+  };
+
+  handleChange = ({ currentTarget: input }) => {
+    const { account } = this.state;
+    account[input.name] = input.value;
+    this.setState({ account });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log("Submitted");
+    this.props.history.push("/home");
+  };
+  render() {
+    const { account } = this.state;
+    return (
+      <div className="form">
+        <h1>Log In</h1>
+        <form onSubmit={this.handleSubmit}>
+          <Input
+            name="username"
+            value={account.username}
+            onChange={this.handleChange}
+            label="Username"
+          />
+
+          <Input
+            name="password"
+            value={account.password}
+            onChange={this.handleChange}
+            label="Password"
+          />
+
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default LogInForm;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <Link to="/" className="navbar-brand">
+        College Forum
+      </Link>
+
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon" />
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/">
+              Home
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/news">
+              News
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/login">
+              Log In
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
